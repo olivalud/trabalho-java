a86fa74c61d2f5429171ff3715b5019c709205c8
Lud Oliva <olivalud12@gmail.com>
2024-06-19 21:28:31 -0300

    Pessoa.java

--- trabalho-java/Pessoa.java
+++ trabalho-java/Pessoa.java
@@ -4,13 +4,14 @@ public class Pessoa {
   protected int age;
   protected String email;
 
+// Construtor
   public Pessoa(String name, int age, String email) {
     this.name = name;
     this.age = age;
     this.email = email;
   }
 
-  // SOBRECARGA dos métodos de atualizar as infos
+  // Sobrecarga dos métodos de atualizar as infos
   public void atualizarInfo(String name) {
     this.name = name;
   }
@@ -25,7 +26,5 @@ public class Pessoa {
 }
 
 // Construtor - Inicializa os atributos
-// Atributos - Nome, idade e email. ( Name, age, email )
-// Métodos 'atualizarInfo' 'atualizarEmail' - 3 métodos SOBRECARREGADOS para
-// atualizar name, age,
-// email.
+// Atributos - Nome, idade e email. ( name, age, email )
+// Métodos 'atualizarInfo' 'atualizarEmail' - 3 métodos SOBRECARREGADOS para atualizar ( name, age, email.
